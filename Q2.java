e7925e6fb76e6651c1607d38c03da304af995f9c
UnnatiRana18 <ranaunnati223@gmail.com>
2025-11-12 23:15:09 +0530

    java

---
+++ Q2.java
@@ -0,0 +1,16 @@
+//take 2 variables and print their sum
+import java.util.*;
+
+public class Q2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        // Calculate the sum of the two variables
+        int sum = a + b;
+
+        // Print the result
+        System.out.println( sum);
+    } 
+}
---
+++ datatypes.java
@@ -0,0 +1,12 @@
+public class datatypes {
+    public static void main(String[] args) {
+        // This class is a placeholder for datatype examples
+        System.out.println("This is the datatypes class.");
+
+        int a=10;
+        int b=20;
+        int sum = a + b;
+        System.out.println("Sum of a and b is: " + (a + b));
+        System.out.println(sum);
+    }
+}
