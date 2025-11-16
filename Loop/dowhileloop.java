704afaa04da6e0b2411031f2a41b0b6fbe3d9ac4
UnnatiRana18 <ranaunnati223@gmail.com>
2025-11-16 13:01:48 +0530

    commit

--- Loop/dowhileloop.java
+++ Loop/dowhileloop.java
@@ -1,14 +1,11 @@
-
-
-public class whileloop {
+public class dowhileloop {
     public static void main(String[]args){
     int i=0;
     System.out.println(i);
 
-    while(i<11){
+    do{
         System.out.println(i);
         i++;
-    }
-    
+    }while(i<11);
 }
 }
