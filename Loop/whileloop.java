054fd9c360112646c32586d7d37aced790639c6c
UnnatiRana18 <ranaunnati223@gmail.com>
2025-11-16 13:01:13 +0530

    commit

---
+++ Loop/whileloop.java
@@ -0,0 +1,14 @@
+package Loop;
+
+public class whileloop {
+    public static void main(String[]args){
+    int i=0;
+    System.out.println(i);
+
+    while(i<11){
+        System.out.println(i);
+        i++;
+    }
+    
+}
+}
