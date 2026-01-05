284ddcf97abf40617aa23b46dcdbeeee4dc13394
UnnatiRana18 <ranaunnati223@gmail.com>
2026-01-05 13:28:31 +0530

    m

---
+++ Sudoku.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class Sudoku{
+
+    public static wrapper_sudoku(int [][]grid,int row,int col){
+        //boolean [][]isempty=false;
+        int dim=row.length;
+        for(row=0;row<dim;row++){
+            for(col=0;col<dim;col++){
+
+                if( grid[row][col]==0){
+
+                    if(isGood)
+
+
+                    //isempty=true;
+                }
+            
+        }
+    }
+    /* for(row=0;row<dim;row++){
+            for(col=0;col<dim;col++){
+                if( isempty==true){
+                    if(choicedigit )
+                }
+
+*/
+
+    }
+
+    public static helper_sudoku( int [][]original,int )
+    public static isGood(int [][]grid,int row,int col, int choiceDigit){
+       //row
+        for(int i=0;i<9;i++){
+            grid[row][i] == choiceDigit;
+
+        }
+
+
+
+
+    }
+public static void Main(String [] args){
+    int row;
+    int col;
+    int dim=row.length;
+
+    int grid[row][col] = {
+                      {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                      {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                      {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                      {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                      {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                      {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                      {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                      {0, 0, 5, 2, 0, 6, 3, 0, 0}
+                      };
+
+
+    if()
+
+}
+}
---
+++ input/name.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class name{
+public static void  main(String args[]){
+
+    Scanner kbrd = new Scanner(System.in);
+
+    String name=kbrd.nextLine();
+
+    System.out.println("My Name is "+name);
+}u
+}
