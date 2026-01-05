7afce60bcfb4382fbe28eb42797d5dbf30417c24
UnnatiRana18 <ranaunnati223@gmail.com>
2026-01-05 13:39:03 +0530

    M

---
+++ input/child.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class child {
+    public static void main(String args[]){
+
+        Scanner kbrd=new Scanner(System.in);
+
+        String myname=kbrd.nextLine();
+        String mother=kbrd.nextLine();
+        String father=kbrd.nextLUine();
+
+        System.out.println("My name is "+myname+" and I am a proud child of "+father+" and "+mother);
+    }
+    
+}
