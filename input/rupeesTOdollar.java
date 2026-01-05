61b4ae312ecfa5e9fc938506366425cb3b3cf7bc
UnnatiRana18 <ranaunnati223@gmail.com>
2026-01-05 23:32:39 +0530

    m

---
+++ input/rupeesTOdollar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class rupeesTOdollar {
+    public static void main(String args[]){
+
+        Scanner kbrd=new Scanner(System.in);
+
+        double rupees = kbrd.nextDouble();
+        double unit=kbrd.nextDouble();
+
+        
+    }
+    
+}
