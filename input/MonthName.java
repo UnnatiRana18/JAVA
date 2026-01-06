c93a3437135ee96028ab30b28ba10f6147a0f9e7
UnnatiRana18 <ranaunnati223@gmail.com>
2026-01-06 10:50:55 +0530

    m

---
+++ input/MonthName.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class MonthName {
+    public static void main(String args[]){
+
+        Scanner kbrd = new Scanner(System.in);
+
+        int num= kbrd.nextInt();
+
+
+    /*Chaitra)
+(Vaishakh)
+ (Jyeshtha)
+ (Ashadha)
+ (Shravana)
+ (Bhadrapada)
+ (Ashwin)
+ (Kartik)
+ (Margashirsha)
+ (Paush)
+ (Magh)
+ (Phalguna
+ */
+
+if(num==1){
+    System.out.println("Chaitra " + "-" +" Hindi");
+}
+else if(num==2){
+    System.out.println("Vaishakh " + "-" +" Hindi");
+}
+else if(num==3){
+    System.out.println("Jyeshtha " + "-" +" Hindi");
+}
+else if(num==4){
+    System.out.println("Ashadha " + "-" +" Hindi");
+}
+else if(num==5){
+    System.out.println("Shravana " + "-" +" Hindi");
+}
+else if(num==6){
+    System.out.println("Bhadrapada " + "-" +" Hindi");
+}
+else if(num==7){
+    System.out.println("Ashwin " + "-" +" Hindi");
+}
+else if(num==8){
+    System.out.println("Kartik " + "-" +" Hindi");
+}
+else if(num==9){
+    System.out.println("Margashirsha " + "-" +" Hindi");
+}
+else if(num==10){
+    System.out.println("Paush " + "-" +" Hindi");
+}
+else if(num==11){
+    System.out.println("Magh " + "-" +" Hindi");
+}
+else if(num==12){
+    System.out.println("Phalguna " + "-" +" Hindi");
+}
+
+    }
+    
+}
